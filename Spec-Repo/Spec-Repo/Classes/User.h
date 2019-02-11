4405c2d55133f8d18982deae580c64e2f0b8ff6d
mac <mac@macdeMacBook-Pro.local>
2019-02-11 15:43:40 +0800

    Add classes

---
+++ Spec-Repo/Spec-Repo/Classes/User.h
@@ -0,0 +1,17 @@
+//
+//  User.h
+//  Spec-Repo
+//
+//  Created by mac on 2019/2/11.
+//  Copyright © 2019年 mac. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface User : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Spec-Repo/Spec-Repo/Classes/User.m
@@ -0,0 +1,13 @@
+//
+//  User.m
+//  Spec-Repo
+//
+//  Created by mac on 2019/2/11.
+//  Copyright © 2019年 mac. All rights reserved.
+//
+
+#import "User.h"
+
+@implementation User
+
+@end
